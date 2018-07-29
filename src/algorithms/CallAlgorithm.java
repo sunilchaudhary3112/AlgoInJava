44cc4489f03d20919885d2d5189c3cc02e2ba002
sunil <sunilchaudhary3112>
2018-07-29 23:03:42 +0530

    First commit to set repo

---
+++ src/algorithms/CallAlgorithm.java
@@ -0,0 +1,10 @@
+package algorithms;
+
+public class CallAlgorithm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test java Algorithms Program");
+	}
+
+}
